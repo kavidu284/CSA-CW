4fadeb124d2d7b25988e1309f8ed305e248ccf00
kavidu <wijenayakakavidu@gmail.com>
2026-04-18 17:49:40 +0530

    fix run issue

--- src/main/java/com/csa/smartcampus/Main.java
+++ src/main/java/com/csa/smartcampus/Main.java
@@ -1,14 +1,15 @@
 package com.csa.smartcampus;
 
-import com.csa.smartcampus.config.AppConfig;
+import java.net.URI;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
-import java.net.URI;
+import com.csa.smartcampus.config.AppConfig;
 
 public class Main {
 
-    public static final String BASE_URI = "http://localhost:8080/";
+    public static final String BASE_URI = "http://localhost:8080/api/v1/";
 
     public static void main(String[] args) {
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
@@ -16,7 +17,7 @@ public class Main {
                 new AppConfig()
         );
 
-        System.out.println("Server started at: http://localhost:8080/api/v1");
+        System.out.println("Server started at: " + BASE_URI);
 
         try {
             Thread.currentThread().join();
